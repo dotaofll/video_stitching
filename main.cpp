245670ab9a46a4d8c7d4c8135347684a457651f2
Yuzhe Liu <dotaofll@163.com>
2019-12-08 18:32:00 +0800

    Enable Thread function but need to be tested

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
-#include  <opencv2/opencv.hpp>
-#include  <iostream>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <memory>
+#include <array>
+#include <string_view>
+#include "./Thread/ThreadPool.h"
 #include "RadialStitcher.h"
 
-#define IMAGEWIDTH 640
-#define IMAGEHEIGHT 480
-#define NUM_IMAGES 3   // 线程数量
+constexpr auto IMAGEWIDTH = 640;
+constexpr auto IMAGEHEIGHT = 480;
+constexpr auto NUM_IMAGES = 3;
+
 
 using namespace std;
 using namespace cv;
@@ -12,58 +17,71 @@ using namespace cv;
 
 
 
-VideoCapture read_camera(string cam_id){
-    //读取视频或摄像头
-    VideoCapture capture(cam_id);
-    capture.set(cv::CAP_PROP_FPS, 1);
-    return  capture;
+VideoCapture read_camera(string cam_id) {
+	//读取视频或摄像头
+	VideoCapture capture(cam_id);
+	capture.set(cv::CAP_PROP_FPS, 1);
+	return  capture;
 }
 
 //这里需要写一个线程　用队列直接传frames 4个队列 每个队列里面有一个线程用于拼接
 // python 样例　imgs_original = [q.get() for q in queue_list]
-void collect_capture(vector<VideoCapture> captures){
-    vector<Mat> img_list;
-    auto *rs = new RadialStitcher(NUM_IMAGES);
-    Rect rect(19, 0, 602, 480);
-    vector<Mat> cylinder_maps = rs->cylinder_projection_map(640, 480, 720);  //calculate the mapping of the cylinder
-    while (true)
-    {
-        for (int i = 0; i < 4; ++i) {
-            Mat frame; Mat cylinder;
-            captures[i] >> frame;
-            cv::remap(frame, cylinder, cylinder_maps[0], cylinder_maps[1],cv::INTER_LINEAR, BORDER_CONSTANT, Scalar(0, 0, 0));
-            cylinder = cylinder(rect); //remove the blank edge of the cylinder image
-            img_list.push_back(cylinder);
-        }
-        Mat result = rs->Stitch(img_list);
-        imshow("result", result);
-        img_list.clear();
-        if ((cv::waitKey(25) & 0XFF) == 27) break;
-    }
-    destroyAllWindows();
-    for (int i = 0; i < 4; ++i) {
-        captures[i].release();
-    }
-}
 
 int main()
 {
+	std::array<string_view, 4> cam_idx{ "/home/eksan/Desktop/test.mp4",
+		"/home/eksan/Desktop/test.mp4",
+		"/home/eksan/Desktop/test.mp4",
+		"/home/eksan/Desktop/test.mp4" };
 
-    string cam_idx[4] = {"/home/eksan/Desktop/test.mp4", "/home/eksan/Desktop/test.mp4", "/home/eksan/Desktop/test.mp4", "/home/eksan/Desktop/test.mp4"};
-//      string cam_idx[4] = { "/dev/v4l/by-path/pci-0000:00:14.0-usb-0:3:1.0-video-index0",
-//                          "/dev/v4l/by-path/pci-0000:00:14.0-usb-0:5:1.0-video-index0",
-//                          "/dev/v4l/by-path/pci-0000:05:00.0-usb-0:1:1.0-video-index0",
-//                          "/dev/v4l/by-path/pci-0000:00:14.0-usb-0:4:1.0-video-index0",};
+	stiching::ThreadPool pool(NUM_IMAGES);
 
-    VideoCapture capture1 = read_camera(cam_idx[0]);
-    VideoCapture capture2 = read_camera(cam_idx[1]);
-    VideoCapture capture3 = read_camera(cam_idx[2]);
-    VideoCapture capture4 = read_camera(cam_idx[3]);
-    vector<VideoCapture> captures; captures.push_back(capture1); captures.push_back(capture2); captures.push_back(capture3); captures.push_back(capture4);
-    collect_capture(captures);
+	vector<VideoCapture> captures;
+	vector<Mat> frameVec(4, Mat());
+	vector<Mat> img_list;
 
+	for (int i = 0; i < 4; i++) {
+		pool.async([&]() {
+			captures.push_back(std::move
+			(read_camera
+			(cam_idx[i].data())));
+			});
+	}
 
+	for (int i = 0; i < 4; i++) {
+		pool.async([&]() {
+			Mat frame;
+			captures[i] >> frame;
+			frameVec.push_back(frame);
+			});
+	}
 
-       return  0;
+	std::shared_ptr<RadialStitcher> rs = make_shared<RadialStitcher>
+		(new RadialStitcher(NUM_IMAGES));
+	Rect rect(19, 0, 602, 480);
+	vector<Mat> cylinder_maps = rs->cylinder_projection_map(640, 480, 720);  //calculate the mapping of the cylinder
+
+	for (int i = 0; i < 4; i++) {
+		pool.async([&]() {
+			Mat cylinder;
+			cv::remap(frameVec[i], cylinder,
+				cylinder_maps[0], cylinder_maps[1],
+				cv::INTER_LINEAR, BORDER_CONSTANT, Scalar(0, 0, 0));
+			cylinder = cylinder(rect); //remove the blank edge of the cylinder image
+			img_list.push_back(cylinder);
+			});
+	}
+
+	Mat result = rs->Stitch(img_list);
+	imshow("result", result);
+	img_list.clear();
+	if ((cv::waitKey(25) & 0XFF) == 27) exit(1);
+
+	cv::destroyAllWindows();
+	for (int i = 0; i < 4; ++i) {
+		captures[i].release();
+	}
+
+	return  0;
 }
 
